a56b82683b9dc45ab4dcf3471abc91ef13212e36
youngsterxyf <sas.198708@gmail.com>
2014-08-21 01:50:10 +0800

    更新

--- leveldb/code/load_data/load_data.py
+++ leveldb/code/load_data/load_data.py
@@ -7,12 +7,14 @@ import requests
 
 def main():
     charSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-    for _ in xrange(1000000):
+    for index in xrange(1000000):
+        print index
         key = ''.join(random.sample(charSet, 20))
         value = ''.join(random.sample(charSet, 30))
         resp = requests.get('http://127.0.0.1:8799/leveldb?action=set&key=' + key + '&value=' + value)
         print resp.status_code
-
+        if resp.status_code == 200:
+            print resp.json()
 
 if __name__ == '__main__':
     main()
